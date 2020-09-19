093d57ab0f84ebe38c398d88a040bb5f2afa206c
Manjeet Singh Nathawat <manjeet@apttus.com>
2020-09-19 17:56:44 +0530

    fafa

--- src/test/java/com/infocrats/runner/TestRunner.java
+++ src/test/java/com/infocrats/runner/TestRunner.java
@@ -6,10 +6,6 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
-<<<<<<< HEAD
-=======
-
->>>>>>> 3605dbd12bb5eb59cbae8e775970cb27e21dbcfd
 @CucumberOptions(features = { "src/test/resources/features" }, tags = { "@RunMSN" }, glue = {
 		"com.infocrats.stepdefs" }, monochrome = true, plugin = { "pretty", "html:target/cucumber-reports",
 
