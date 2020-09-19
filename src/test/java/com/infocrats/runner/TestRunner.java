9b3d2bd9f88bbdddbc92a9344bf3af44a94e1a08
Manjeet Singh Nathawat <manjeet@apttus.com>
2020-09-19 18:40:18 +0530

    ss

---
+++ src/test/java/com/infocrats/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package com.infocrats.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@RunMSN" }, glue = {
+		"com.infocrats.stepdefs" }, monochrome = true, plugin = { "pretty", "html:target/cucumber-reports",
+
+				"json:target/cucumber.json", "rerun:rerun.txt" })
+public class TestRunner {
+
+}
