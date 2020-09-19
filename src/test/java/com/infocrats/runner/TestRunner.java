3605dbd12bb5eb59cbae8e775970cb27e21dbcfd
Manjeet Singh Nathawat <manjeet@apttus.com>
2020-09-19 17:22:45 +0530

    ff

--- src/test/java/com/infocrats/runner/TestRunner.java
+++ src/test/java/com/infocrats/runner/TestRunner.java
@@ -6,12 +6,8 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
-<<<<<<< Updated upstream
 
-@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@Passed" }, glue = {
-=======
-@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@LeaveManagementEmpTermite" }, glue = {
->>>>>>> Stashed changes
+@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@RunMSN" }, glue = {
 		"com.infocrats.stepdefs" }, monochrome = true, plugin = { "pretty", "html:target/cucumber-reports",
 
 				"json:target/cucumber.json", "rerun:rerun.txt" })
