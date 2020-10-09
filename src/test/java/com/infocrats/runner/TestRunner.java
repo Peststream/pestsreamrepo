f3cacbf9584c97ad66a5087ba52a9b1cb97f6fc1
jsingh <jitendra_apttus@bitbucket.org>
2020-10-09 11:27:55 +0530

    no message

--- src/test/java/com/infocrats/runner/TestRunner.java
+++ src/test/java/com/infocrats/runner/TestRunner.java
@@ -7,7 +7,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
 
-@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@sendEmailInsideRadioBttn_Com" }, glue = {
+@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@hvac" }, glue = {
 		"com.infocrats.stepdefs" }, monochrome = true, plugin = { "pretty", "html:target/cucumber-reports",
 
 				"json:target/cucumber.json", "rerun:rerun.txt" })
